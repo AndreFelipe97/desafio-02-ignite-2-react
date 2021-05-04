fe04f1f71e5b0311b58f3d95185498873ba99e0f
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2021-05-04 14:20:45 -0300

    criado a lógica do sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { useState, useEffect } from 'react';
+import { api } from '../services/api';
+
+// Interfaces
+import { GenreResponseProps } from '../interfaces/GenreResponseProps';
+
+// Components
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: GenreResponseProps[],
+  click: any,
+  genreId: number,
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.click(genre.id)}
+            selected={props.genreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
